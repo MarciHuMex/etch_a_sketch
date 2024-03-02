729848a0634be724c694ba40b16caefc59c42128
MarciHuMex <151879261+MarciHuMex@users.noreply.github.com>
2024-03-02 20:54:25 +0100

    add flexible amount of grids

--- etch.js
+++ etch.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {   
     const container = document.getElementById("container");
+    const gridSize =60;
+    const flexBasis = 100 / gridSize;
 
-    for (let i = 0; i < 16 * 16 ; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
+        gridItem.style.flex = `1 0 ${flexBasis}%`; // Corrected backticks here
         container.appendChild(gridItem);
     }
 });
+
