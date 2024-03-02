8f25b1a3f73d37b6601f518db586fb452c05d4f6
MarciHuMex <151879261+MarciHuMex@users.noreply.github.com>
2024-03-02 18:44:40 +0100

    create grid, half done

---
+++ etch.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {   
+    const container = document.getElementById("container");
+
+    for (let i = 0; i < 16 * 16 ; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        container.appendChild(gridItem);
+    }
+});
